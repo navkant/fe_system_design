6841850606ec27c5ebb2e26d6e430a5169bd00a0
Navkant <navkant.tyagi9@gmail.com>
2025-10-18 20:01:54 +0530

    added sse implementation in python

--- communication_techniques/sse/python/index.js
+++ communication_techniques/sse/python/index.js
@@ -1,10 +1,18 @@
-console.log("script executed")
-const eventSource = new EventSource("http://13.53.32.8/test_route")
+console.log("script executed");
+const eventSource = new EventSource("http://localhost:8080/sse");
+const eventList = document.getElementById("events-list");
+
 eventSource.onmessage = (event) => {
-    console.log("Received message:", event.data )
-}
-eventSource.addEventListener('ENDSTREAM', function(event) {
-    console.log('End Stream recieved closing eventsource', event.data);
-    eventSource.close();
+  console.log("Received message:", typeof JSON.parse(event.data));
+  const listItem = document.createElement("li");
+  listItem.textContent = `${JSON.parse(event.data).message}`;
+  eventList.appendChild(listItem);
+};
+eventSource.addEventListener("ENDSTREAM", function (event) {
+  console.log("End Stream recieved closing eventsource", event.data);
+  eventSource.close();
+  const listItem = document.createElement("li");
+  listItem.textContent = `Message: ${event.data}`;
+  eventList.appendChild(listItem);
 });
-console.log(eventSource)
+console.log(eventSource);
--- communication_techniques/sse/python/main.py
+++ communication_techniques/sse/python/main.py
@@ -10,95 +10,17 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 CORS(app, supports_credentials=True)
 
 
-@app.route("/", methods=["GET"])
-def index():
-    # if request.method == "OPTIONS": # CORS preflight
-    #     return _build_cors_preflight_response()
-    print(request.cookies)
-    time.sleep(4)
-
-    if "count" not in session:
-        session["count"] = 1
-    else:
-        session["count"] += 1
-
-    return jsonify(
-        {"message": "Hello world!",
-         "status": 1,
-         "visit_count": session["count"]
-         }
-    )
-
-
-# @app.after_request
-# def set_session_cookie(response):
-#     # Check if a session cookie already exists
-#     if "session" in request.cookies:
-#         return response
-
-#     # Set the session cookie
-#     breakpoint()
-#     response.set_cookie("session", session, httponly=True)
-#     return response
-
-
-# def _build_cors_preflight_response():
-#     response = make_response()
-#     response.headers.add("Access-Control-Allow-Origin", "127.0.0.1")
-#     response.headers.add('Access-Control-Allow-Headers', "*")
-#     response.headers.add('Access-Control-Allow-Methods', "*")
-#     return response
-
-
-@app.route("/restaurants/", methods=["GET"])
-def restaurant_list():
-    rest_list_data = json.load(open("rest_list.json", "r"))
-    for item in rest_list_data["data"]["cards"]:
-        if "id" in item["card"]["card"].keys():
-            if item["card"]["card"]["id"] == "top_brands_for_you":
-                rest_list = item["card"]["card"]["gridElements"]["infoWithStyle"]["restaurants"]
-
-    return jsonify(
-        {
-            "status": 1,
-            "restaurant_list": rest_list
-        }
-    )
-
-
-@app.route("/restaurant/<restaurant_id>/")
-def get_restaurant(restaurant_id):
-    rest_list_data = json.load(open("rest_list.json", "r"))
-    for item in rest_list_data["data"]["cards"]:
-        if "id" in item["card"]["card"].keys():
-            if item["card"]["card"]["id"] == "top_brands_for_you":
-                rest_list = item["card"]["card"]["gridElements"]["infoWithStyle"]["restaurants"]
-
-    for restaurant in rest_list:
-        if restaurant["info"]["id"] == restaurant_id:
-            return jsonify(
-                {
-                    "status": 1,
-                    "restaurant_data": restaurant,
-                }
-            )
-        else:
-            continue
-
-    return jsonify(
-        {
-            "status": 0,
-            "message": "restaurant  not found"
-        }
-    )
-
-@app.route("/test_route")
+@app.route("/sse")
 def test_route():
     def generate_events():
-        messages = [{"message": "Hello", "status": 0}, {"message": "from", "status": 0}, {"message": "Flask SSE!", "status": 1}]
-        for msg in messages:
-            time.sleep(40)  # Simulate some processing delay
-            yield f"data: {msg}\n\n"
+        messages = [
+            {"message": "Connection successfull", "status": 0},
+            {"message": "First message", "status": 0}, 
+            {"message": "Second message", "status": 0}
+        ]
+        for index, msg in enumerate(messages):
+            yield f"data: {json.dumps(msg)}\n\n"
+            time.sleep(5)  # Simulate some processing delay
 
         end_stream = ["event: ENDSTREAM\n", "data: This is the end of stream\n\n"]
         yield end_stream[0]
