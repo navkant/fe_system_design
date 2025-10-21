ffbe0cfc55583d01ebf4e58de324190a37d1fb0b
navkant <navkant.tyagi9@gmail.com>
2025-10-21 17:45:20 +0530

    improvements to e2e.js

--- testing/e2e.js
+++ testing/e2e.js
@@ -3,8 +3,15 @@ const puppeteer = require("puppeteer");
 (async () => {
     const browser = await puppeteer.launch({
         headless: false,
+        args: ["--window-size=1920,1080"],
     });
     const page = await browser.newPage();
     await page.goto("https://namastedev.com/");
     console.log("webpage loaded");
+    await page.setViewport({ width: 1920, height: 1080 });
+
+    const coursePageLink = ".px-1 > li:nth-child(2) > a";
+    await page.waitForSelector(coursePageLink);
+    await page.click(coursePageLink);
 })();
+  
