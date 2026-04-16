2a2e47ff7efb4fbae7c442fc5be9db564b0046aa
Navkant Tyagi <navkant.tyagi9@gmail.com>
2026-04-16 19:17:51 +0530

    added database and caching concepts

---
+++ database_caching/httpCaching/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "public, max-age=86400");
+  // res.setHeader('Cache-Control', 'public, max-age=86400');
+  res.setHeader("Expires", "Sat, 16 Apr 2026 13:43:39 GMT");
+  next();
+});
+
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    etag: false,
+    cacheControl: false,
+    lastModified: false,
+  }),
+);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
